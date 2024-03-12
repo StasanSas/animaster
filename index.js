7433f443c915364f19d46e37ff6200d36a8ab480
Stas <stanislav.ivanov.2004@internet.ru>
2024-03-12 19:34:38 +0500

    ghfbdz

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 addListeners();
 
+let idHeartBit =0;
+let obj = undefined;
+moveAndHide = 0;
 function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
@@ -7,10 +10,17 @@ function addListeners() {
             animaster().fadeIn(block, 5000);
         });
 
+
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
             animaster().move(block, 1000, {x: 100, y: 10});
+        });
+
+    document.getElementById('movePlay2')
+        .addEventListener('click', function () {
+            const block = document.getElementById('moveBlock');
+            animaster().addMove(500, {x: 20, y:20}).play(block);
         });
 
     document.getElementById('scalePlay')
@@ -30,12 +40,36 @@ function addListeners() {
             const block = document.getElementById('moveAndHideBlock');
             animaster().moveAndHide(block, 5000);
         });
+
+    document.getElementById('moveAndHideReset')
+        .addEventListener('click', function () {
+            const block = document.getElementById('moveAndHideBlock');
+            animaster().resetMove(block);
+        });
+
+    document.getElementById('showAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('showAndHideBlock');
+            animaster().showAndHide(block, 5000);
+        });
+    document.getElementById('heartBeatingPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('heartBeatingBlock');
+            obj = animaster().heartBeating(block, 5000);
+            obj.start();
+        });
+    document.getElementById('heartBeatingStop')
+        .addEventListener('click', function () {
+            const block = document.getElementById('heartBeatingBlock');
+            obj.stop();
+        });
 }
 
 
 
 
 function animaster() {
+    let _steps = [];
     return {
         /**
          * Функция, увеличивающая/уменьшающая элемент
@@ -82,9 +116,90 @@ function animaster() {
 
         moveAndHide (element, duration) {
             this.move(element, 2 * (duration/5), {x: 100, y:20});
-            setTimeout(() => {
+            moveAndHide = setTimeout(() => {
                 this.fadeOut(element, 3 * (duration/5));
             }, 2 * (duration/5));
+        },
+
+        heartBeating (element, duration) {
+            const o = this;
+            return {
+                start(){
+                    idHeartBit = setInterval(() => {
+                        setTimeout(() => {
+                            o.scale(element, 250, 1.4)
+                        }, 0);
+                        setTimeout(() => {
+                            o.scale(element, 250, 1)
+                        }, 250);
+                    }, 500);
+                },
+                stop(){
+                    clearInterval(idHeartBit);
+                }
+            }
+             // выводит 0, затем 1, затем 2
+
+        },
+
+        showAndHide (element, duration) {
+            this.fadeIn(element, duration/3);
+            setTimeout(() => {
+                this.fadeOut(element, (duration/3));
+            }, 2 * (duration/3));
+        },
+
+        resetFadeIn(element) {
+            element.style.transitionDuration = null;
+            element.classList.remove('show');
+            element.classList.add('hide');
+        },
+
+        resetFadeOut(element) {
+            element.style.transitionDuration = null;
+            element.classList.remove('hide');
+            element.classList.add('show');
+        },
+
+        resetMoveAndScale(element) {
+            element.style.transitionDuration = null;
+            element.style.transform = null;
+        },
+
+        resetMove(element){
+            this.resetMoveAndScale(element);
+            this.resetFadeOut(element);
+            clearInterval(moveAndHide);
+        },
+
+        addMove(duration, translation) {
+            _steps.push([this.move, duration, translation]);
+            return this;
+        },
+
+        addScale(duration, translation) {
+            _steps.push([this.scale, duration, ratio]);
+            return this;
+        },
+
+        addFadeIn(duration, translation) {
+            _steps.push([this.fadeIn, duration]);
+            return this;
+        },
+
+        addFadeOut(duration, translation) {
+            _steps.push([this.fadeOut, duration]);
+            return this;
+        },
+
+        play(element) {
+            let totalTime = 0;
+            for (const data of _steps) {
+                let [func, ...args] = data;
+                args.unshift(element);
+                totalTime += args[1];
+                setTimeout(() => func.apply(null, args), totalTime);
+            }
         }
 
     };
